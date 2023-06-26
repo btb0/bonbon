c0f91e1ab7bb5de0db4adadf3bf523cd5adc2e2e
btb0 <benburke2016@gmail.com>
2023-06-26 18:53:58 -0400

    adds category model & removes styling

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -15,7 +15,6 @@ export default function App() {
           <NavBar user={user} setUser={setUser} />
           <Routes>
             {/* Route components in here */}
-
           </Routes>
         </>
         :
