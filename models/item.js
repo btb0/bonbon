70103fd965b7887e2d193be0b3ea2f1797e5d446
btb0 <benburke2016@gmail.com>
2023-06-26 21:18:02 -0400

    item model and temp seed data

---
+++ models/item.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  picture: { type: String, required: true },
+  category: { type: String, required: true},
+  price: { type: Number, required: true }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Item', itemSchema);
