38fd19b0780c4fbf9cc347dc5f9682143de285a1
btb0 <benburke2016@gmail.com>
2023-07-04 17:51:12 -0400

    item detail styling

--- src/pages/ItemDetailPage/ItemDetailPage.jsx
+++ src/pages/ItemDetailPage/ItemDetailPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import * as itemsAPI from '../../utilities/items-api';
 import * as ordersAPI from '../../utilities/orders-api';
+import './ItemDetailPage.css';
 
 export default function ItemDetailPage({ cart, setCart }) {
   const [selectedItem, setSelectedItem] = useState({});
@@ -23,14 +24,23 @@ export default function ItemDetailPage({ cart, setCart }) {
   }
   
   return (
-    <main>
-      <h1>{selectedItem.name}</h1>
-      <p>SKU: {selectedItem.itemSKU}</p>
-      <p>{selectedItem.description}</p>
-      <p>{selectedItem.price?.toFixed(2)}</p>
-      <p>{selectedItem.location}</p>
-      <img src={selectedItem.picture} alt={selectedItem.name} />
-      <button onClick={() => handleAddToCart(itemId)}>Add to Cart</button>
+    <main className='ItemDetailPage'>
+      <div className="details-container columns">
+        <section className='details-image column is-half'>
+          <img src={selectedItem.picture} alt={selectedItem.name} />
+        </section>
+        <section className='details-info column auto'>
+          <p className='right'>SKU: {selectedItem.itemSKU}</p>
+          <h1>{selectedItem.name}</h1>
+          <h2>${selectedItem.price?.toFixed(2)}</h2>
+          <h5>Location: &nbsp;{selectedItem.location}</h5>
+          <p>{selectedItem.description}</p>
+          <button 
+            className='button is-rounded is-responsive' 
+            onClick={() => handleAddToCart(itemId)}
+          >Add to Cart</button>
+        </section>
+      </div>
     </main>
   );
 }
