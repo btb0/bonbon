a7b5eece000d77440fcaa31546bdc3ee32936bd7
btb0 <benburke2016@gmail.com>
2023-07-05 12:05:21 -0400

    updates idx cards

--- src/components/IndexItem/IndexItem.jsx
+++ src/components/IndexItem/IndexItem.jsx
@@ -12,47 +12,24 @@ export default function IndexItem({ item, cart, setCart }) {
   }
 
   return (
-    // <div className='IndexItem flex'>
-    //   <div className="img-idx-container flex-ctr-ctr">
-    //     <Link to={`/items/${item._id}`}>
-    //       <img className='product-idx-img' src={item.picture} alt={item.name} />
-    //     </Link>
-    //   </div>
-    //   <Link to={`/items/${item._id}`}>
-    //     <h4>{item.name}</h4>
-    //   </Link>
-    //   <div className="flex price-idx-add">
-    //     <p>${item?.price.toFixed(2)}</p>
-    //     <button onClick={() => handleAddToCart(item._id)}>
-    //       <span class="material-symbols-outlined">add_shopping_cart</span>
-    //     </button>
-    //   </div>
-    // </div>
-    <div className="IndexItem flex-ctr-ctr">
+    <div className="IndexItem flex">
       <div className="img-idx-container flex-ctr-ctr">
         <img className='product-idx-img' src={item.picture} alt={item.name} />
       </div>
-      <Link to={`/items/${item._id}`}>
-        <h4>{item.name}</h4>
-      </Link>
-      <div className='add-cart-con'>
-        <p>${item?.price.toFixed(2)}</p>
-        <button onClick={() => handleAddToCart(item._id)}>
-          <span className="material-symbols-outlined">add_shopping_cart</span>
-        </button>
+      <div className="bottom-idx">
+        <Link to={`/items/${item._id}`}>
+          <h4>{item.name}</h4>
+        </Link>
+        <div className='add-cart-con center flex'>
+            <p>${item?.price.toFixed(2)}</p>
+          {/* <button onClick={() => handleAddToCart(item._id)}>
+            <span className="material-symbols-outlined">add_shopping_cart</span>
+          </button> */}
+          <button className="button is-rounded" onClick={() => handleAddToCart(item._id)}>
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   );
-
-  
-  //   <div className='IndexItem'>
-  //     <Link to={`/items/${item._id}`}>
-  //       <img className='product-idx-img' src={item.picture} alt={item.name} />
-  //     </Link>
-  //     <Link to={`/items/${item._id}`}>
-  //       <h4>{item.name}</h4>
-  //     </Link>
-  //     <button onClick={() => handleAddToCart(item._id)}>Add to Cart</button>
-  //   </div>
-  // );
 }
