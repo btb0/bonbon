39f305a7df8120e0a05bd27601a5a0ce6a9669c4
btb0 <benburke2016@gmail.com>
2023-07-04 21:58:43 -0400

    temp index card styling

--- src/components/IndexItem/IndexItem.jsx
+++ src/components/IndexItem/IndexItem.jsx
@@ -12,14 +12,24 @@ export default function IndexItem({ item, cart, setCart }) {
   }
 
   return (
-    <div className='IndexItem'>
-      <Link to={`/items/${item._id}`}>
+    <div className='IndexItem flex'>
+      <div className="img-idx-container flex-ctr-ctr">
         <img className='product-idx-img' src={item.picture} alt={item.name} />
-      </Link>
-      <Link to={`/items/${item._id}`}>
-        <h4>{item.name}</h4>
-      </Link>
+      </div>
+      <h4>{item.name}</h4>
       <button onClick={() => handleAddToCart(item._id)}>Add to Cart</button>
     </div>
   );
+
+  
+  //   <div className='IndexItem'>
+  //     <Link to={`/items/${item._id}`}>
+  //       <img className='product-idx-img' src={item.picture} alt={item.name} />
+  //     </Link>
+  //     <Link to={`/items/${item._id}`}>
+  //       <h4>{item.name}</h4>
+  //     </Link>
+  //     <button onClick={() => handleAddToCart(item._id)}>Add to Cart</button>
+  //   </div>
+  // );
 }
